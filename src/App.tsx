71b2bccac5e68d5da226d526c67e7ec97739415d
Aditya Karki <adityakarki201@gmail.com>
2024-09-11 16:42:05 +0530

    Add multiple card

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,19 @@ import CardComp from "./components/CardComp";
 
 function App() {
   return (
-    <div className="flex w-screen h-screen justify-center items-center bg-black">
+    <div className="flex flex-wrap w-screen h-screen justify-center items-center bg-black gap-2">
+      <CardComp
+        name="Aditya Karki"
+        bio="This is a hello world from Aditya Karki"
+      />
+      <CardComp
+        name="Aditya Karki"
+        bio="This is a hello world from Aditya Karki"
+      />
+      <CardComp
+        name="Aditya Karki"
+        bio="This is a hello world from Aditya Karki"
+      />
       <CardComp
         name="Aditya Karki"
         bio="This is a hello world from Aditya Karki"
--- src/components/CardComp.tsx
+++ src/components/CardComp.tsx
@@ -1,4 +1,4 @@
-import { Card } from "antd";
+import { Card, Avatar } from "antd";
 
 interface propTypes {
   name: string;
@@ -7,9 +7,36 @@ interface propTypes {
 
 export default function CardComp({ name, bio }: propTypes) {
   return (
-    <Card title="Card title" bordered={false} style={{ width: 300 }}>
-      <h1>{name}</h1>
-      <p>{bio}</p>
+    <Card
+      title="Profile Card"
+      bordered={true}
+      style={{
+        width: 300,
+        display: "Flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+      hoverable={true}
+      loading={false}
+    >
+      <div className="w-full flex justify-center">
+        <Avatar
+          shape="circle"
+          draggable={true}
+          src={"https://bbdu.ac.in/wp-content/uploads/2021/11/dummy-image1.jpg"}
+          style={{
+            marginBottom: "20px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h1>{name}</h1>
+        <p>{bio}</p>
+      </div>
     </Card>
   );
 }
